66215923d7b9d2560670755b29768251bb6c58c4
thinhisme1123 <kieplulanh93@gmail.com>
2023-04-28 22:20:07 +0700

    test 1

--- FinalTest/Form1.cs
+++ FinalTest/Form1.cs
@@ -6,5 +6,10 @@ namespace FinalTest
         {
             InitializeComponent();
         }
+        // test test
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
